454e3c6861c45203f27e57aca7cc016dde0a8625
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-12-29 11:40:06 +0100

    Add script for VCF to Treemix conversion
    This script sets up a SLURM job to convert VCF files for Treemix analysis, activating the necessary conda environment and specifying input files.

---
+++ vcf_generation_pipeline/Treemix/lancer_vcf2treemix.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p cpu-dedicated
+#SBATCH --account=dedicated-cpu@dgimi-eha
+source /home/durandk/miniconda3/etc/profile.d/conda.sh
+conda activate vcftools
+#############################################
+# /storage/simple/users/durandk/scratch_durandk/GenFAW600/VCF/202512_newvcf/merged.snps.max_5_missing.Treemix.vcf.gz
+#  - Raw vcf rename chrom from 1-29
+#    - Keep variants with at least 95% present genotypes (F_MISSING < 0.05)
+#    --keep variants with a maximum of 5% missing data (F_MISSING < 0.05)
+
+#############################################
+
+
+
+
+# erreue ce fichier n est pas pruned errer dans le nom
+
+sbatch vcf2treemix.sh /storage/simple/users/durandk/scratch_durandk/GenFAW600/VCF/202512_newvcf/merged.snps.max_5_missing.Treemix.vcf.gz pop_treemix.clust
+
+
