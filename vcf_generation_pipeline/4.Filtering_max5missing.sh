be3a1aa26113a021f46f844d00743ce7d03886ce
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-12-29 11:32:11 +0100

    Change output filename for max 5 missing filter

--- vcf_generation_pipeline/4.Filtering_max5missing.sh
+++ vcf_generation_pipeline/4.Filtering_max5missing.sh
@@ -61,8 +61,8 @@ bcftools view \
 #############################################
 
 bcftools view  -i 'F_MISSING < 0.05'  /storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_GenFAW600/2025_GenFAW600/merged.snps.filtered.renamed_chr1-29.miss0.8.vcf.gz  \
- -Oz  -o merged.snps.max_5_missing_pruned.Treemix.vcf.gz --threads 8              
-zcat merged.snps.max_5_missing_pruned.Treemix.vcf.gz | grep -v "#" | wc -l 
+ -Oz  -o merged.snps.max_5_missing.Treemix.vcf.gz --threads 8              
+zcat merged.snps.max_5_missing.Treemix.vcf.gz | grep -v "#" | wc -l 
 
 #############################################
 # 5. Index the resulting VCF files
