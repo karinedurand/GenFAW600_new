dde90984af0db9b1444c0e96035ad37bb7796c00
karine Durand <45596232+karinedurand@users.noreply.github.com>
2026-01-26 09:18:38 +0100

    Update lancer_vcf2treemix.sh

--- vcf_generation_pipeline/Treemix/lancer_vcf2treemix.sh
+++ vcf_generation_pipeline/Treemix/lancer_vcf2treemix.sh
@@ -6,15 +6,9 @@ conda activate vcftools
 #############################################
 # /storage/simple/users/durandk/scratch_durandk/GenFAW600/VCF/202512_newvcf/merged.snps.max_5_missing.Treemix.vcf.gz
 #  - Raw vcf rename chrom from 1-29
-#    - Keep variants with at least 95% present genotypes (F_MISSING < 0.05)
-#    --keep variants with a maximum of 5% missing data (F_MISSING < 0.05)
+#    - Keep variants with at least 95% present genotypes (F_MISSING < 0.05) =  --keep variants with a maximum of 5% missing data (F_MISSING < 0.05)
+############################################
 
-#############################################
-
-
-
-
-# erreue ce fichier n est pas pruned errer dans le nom
 
 sbatch vcf2treemix.sh /storage/simple/users/durandk/scratch_durandk/GenFAW600/VCF/202512_newvcf/merged.snps.max_5_missing.Treemix.vcf.gz pop_treemix.clust
 
