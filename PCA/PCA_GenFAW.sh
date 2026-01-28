7745a9d198675053c6b034a5a7d0e13d99d482ff
karine Durand <45596232+karinedurand@users.noreply.github.com>
2026-01-28 10:34:36 +0100

    Update PCA_GenFAW.sh

--- PCA/PCA_GenFAW.sh
+++ PCA/PCA_GenFAW.sh
@@ -14,11 +14,11 @@ conda activate plink2
 # PCA #--geno 0.05: eliminates SNPs with more than 5% missing, retaining those with at least 95% of data present.
 
 # ==============================
-plink2 --vcf  /home/durandk/scratch_durandk/GenFAW600/VCF/202512_newvcf/GenFAW_max5miss_pruned.vcf.gz \
+plink2 --vcf  /storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_GenFAW600/2025_GenFAW600/VCF_pruned_for_PCA_Admixture/WholeGenome_biallelic_max5miss_pruned.vcf.gz  \
 	  --chr-set 29 \
 	  --allow-extra-chr \
 	  --pca \
-	  --out GenFAW_max5miss_pruned_PCA
+	  --out WholeGenome_biallelic_max5miss_pruned_PCA
 
 
 
@@ -38,6 +38,5 @@ plink2 --vcf  /storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/202
 
 plink2 --vcf  /storage/simple/projects/faw_adaptation/Data_Backup/Merged_vcf/2025_GenFAW600/2025_GenFAW600/VCF_pruned_for_PCA_Admixture/Z_biallelic_max5miss_pruned.vcf.gz \
 	  --allow-extra-chr \
-	  --chr-set 29 \
 	  --pca \
 	  --out Z_biallelic_max5miss_pruned_PCA
